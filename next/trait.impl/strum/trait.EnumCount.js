cbd1db4bad3ba317964193cdc066ec4f9fa38e47
arqunis <arqunis@users.noreply.github.com>
2025-12-13 18:39:18 +0000

    deploy: 240d0e610cc7004926acba260bc2546cb0547eac

--- next/trait.impl/strum/trait.EnumCount.js
+++ next/trait.impl/strum/trait.EnumCount.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["serenity",[["impl EnumCount for <a class=\"enum\" href=\"serenity/gateway/client/enum.FullEvent.html\" title=\"enum serenity::gateway::client::FullEvent\">FullEvent</a>"],["impl EnumCount for <a class=\"enum\" href=\"serenity/model/event/enum.Event.html\" title=\"enum serenity::model::event::Event\">Event</a>"]]]]);
+    const implementors = Object.fromEntries([["serenity",[["impl EnumCount for <a class=\"enum\" href=\"serenity/gateway/client/enum.FullEvent.html\" title=\"enum serenity::gateway::client::FullEvent\">FullEvent</a>",0],["impl EnumCount for <a class=\"enum\" href=\"serenity/model/event/enum.Event.html\" title=\"enum serenity::model::event::Event\">Event</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[316]}
+//{"start":59,"fragment_lengths":[320]}
